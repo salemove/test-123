6587fb2c37402a17a16dbc7e3ad2b22fa58d0a57
Gerson Noboa <gerson.noboa@glia.com>
2021-02-17 14:58:29 +0200

    SocketIO in Salemove

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(name: "Moya", url: "https://github.com/Moya/Moya.git", .exact("14.0.0")),
         .package(name: "Macaw", url: "https://github.com/exyte/Macaw.git", .exact("0.9.7")),
         .package(name: "ReactiveSwift", url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .exact( "6.5.0")),
-        .package(name: "SocketIO", url: "https://github.com/gersonnoboa/ios-socket-client", .exact( "9.2.0")),
+        .package(name: "SocketIO", url: "https://github.com/salemove/ios-socket-client", .exact( "9.2.0")),
         .package(name: "SwiftPhoenixClient", url: "https://github.com/davidstump/SwiftPhoenixClient.git", .exact("1.2.1")),
         .package(name: "TwilioVoice", url: "https://github.com/twilio/twilio-voice-ios", .exact( "6.2.0")),
         .package(name: "WebRTC", url: "https://github.com/alexpiezo/WebRTC.git", .exact( "1.1.31567")),
